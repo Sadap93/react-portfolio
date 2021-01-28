3233a03f28ef7814487c708f7f54e6f277337d73
Kovacs Sandor <sada93@citromail.hu>
2021-01-28 15:28:29 +0100

    19. Scroll Animation

--- react-portfolio/src/animation.js
+++ react-portfolio/src/animation.js
@@ -29,16 +29,16 @@ export const titleAnim = {
 };
 
 export const fade = {
-    hidden: {opacity: 0},
+    hidden: { opacity: 0 },
     show: {
         opacity: 1,
-        transition: {ease: "easeOut", duration: 0.75},
+        transition: { ease: "easeOut", duration: 0.75 },
     },
 };
 
 export const photoAnim = {
 
-    hidden: {scale: 1.5, opacity: 0},
+    hidden: { scale: 1.5, opacity: 0 },
     show: {
         scale: 1,
         opacity: 1,
@@ -49,22 +49,33 @@ export const photoAnim = {
     },
 };
 export const lineAnim = {
-    hidden: {width: "0%"},
+    hidden: { width: "0%" },
     show: {
         width: "100%",
-        transition: {duration: 1},
+        transition: { duration: 1 },
     },
 };
 
-export const slider ={
-    hidden: {x: "-130%", skew: "45deg"},
+export const slider = {
+    hidden: { x: "-130%", skew: "45deg" },
     show: {
         x: "100%",
         skew: "0deg",
-        transition: {ease: "easeOut", duration: 1},
+        transition: { ease: "easeOut", duration: 1 },
     },
 };
 export const sliderContainer = {
-    hidden: {opacity: 1},
-    show: {opacity: 1, transition: {staggerChildren: 0.12, ease: "easeOut"}},
+    hidden: { opacity: 1 },
+    show: { opacity: 1, transition: { staggerChildren: 0.12, ease: "easeOut" } },
 };
+
+export const scrollReveal = {
+    hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.4 } },
+    show: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
