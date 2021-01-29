acc1713df1494ecc5ecaaa3a3bd44e1cac2a2115
Kovacs Sandor <sada93@citromail.hu>
2021-01-29 10:10:09 +0100

    23. Mobile Friendly

--- react-portfolio/src/components/Nav.js
+++ react-portfolio/src/components/Nav.js
@@ -49,6 +49,32 @@ const StyledNav = styled.nav`
         padding-left: 10rem;
         position: relative;
     }
+        @media (max-width: 1300px){
+            flex-direction: column;
+            padding: 2rem 1rem;
+            #logo{
+                display: inline-block;
+                margin: 1rem;
+            }
+            ul{
+                padding: 2rem;
+                justify-content: space-around;
+                width: 100%;
+                @media (max-width: 400px){
+                        display: flex;
+                        flex-direction:column;
+                        align-items: center;
+                        padding: 0rem;
+                    }
+                li{
+                    padding: 0rem;
+                    @media (max-width: 400px){
+                     padding: 1rem;
+                    }
+                   
+                }
+            }
+    }
 `;
 
 export default Nav;
